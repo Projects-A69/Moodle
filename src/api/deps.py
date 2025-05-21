fb506b2474ab9a4e5eabc7ee5e65aeaa5b7225b8
DBoychew <dimitur.boychew@abv.bg>
2025-05-21 20:55:42 +0300

    get current user done , auth refractor

--- src/api/deps.py
+++ src/api/deps.py
@@ -1,10 +1,14 @@
 from typing import Generator
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
-from src.database.session import SessionLocal
+from jose import jwt
 from sqlalchemy.orm import Session
+
+from src.database.session import SessionLocal
+from models.models import User
+from core.config import settings
 
 
 def get_db() -> Generator:
@@ -21,7 +25,25 @@ def get_db() -> Generator:
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-# Complete this method to get the current user session from the token
+
 def get_current_user(db: Session = Depends(get_db),
                      token: str = Depends(oauth2_scheme)):
-    pass
+    
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Unauthorized !")
+
+    try:
+        payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except:
+        raise credentials_exception
+
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise credentials_exception
+
+    return user
+
--- src/core/authentication.py
+++ src/core/authentication.py
@@ -2,7 +2,7 @@ from jose import jwt
 import bcrypt
 from models.models import User
 from datetime import datetime, timedelta, timezone
-from api.deps import find_by_email
+from api.deps import get_current_user
 
 SECRET_KEY = "123"
 ALGORITHM = "HS256"
@@ -10,7 +10,7 @@ TOKEN_EXPIRATION_TIME = 60
 
 def create_token(user: User) -> str:
     payload = {
-        "user_id": user.id, #user.id can't be found, this needs change i think 
+        "user_id": user.id,
         "email": user.email,
         "exp": datetime.now(timezone.utc)+ timedelta(minutes = TOKEN_EXPIRATION_TIME)
     }
@@ -20,8 +20,8 @@ def is_authenticated(token: str) -> bool:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email = payload.get("email")
-        user = find_by_email(email)
-        return user is not None and str(user.id) == str(payload.get("user_id")) #here too
+        user = get_current_user(email)
+        return user is not None and str(user.id) == str(payload.get("user_id"))
     except Exception:
         return False
 
@@ -29,6 +29,6 @@ def from_token(token: str) -> User | None:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email = payload.get("email")
-        return find_by_email(email)
+        return get_current_user(email)
     except Exception:
         return None
