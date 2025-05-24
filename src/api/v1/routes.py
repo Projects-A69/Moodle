12aadbf876b0fb277dec1fb124f7c8da90c5a680
Flazer214 <ivanpustovit786@gmail.com>
2025-05-24 14:11:30 +0300

    add to router sections

--- src/api/v1/routes.py
+++ src/api/v1/routes.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from src.api.v1.endpoints import users,admins,courses
+from src.api.v1.endpoints import users,admins,courses, sections
 
 api_router = APIRouter()
 
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(courses.router, prefix="/courses", tags=["courses"])
+api_router.include_router(sections.router, prefix="/sections", tags=["sections"])
 api_router.include_router(admins.router, prefix="/admins", tags=["admins"])
