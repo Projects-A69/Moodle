ae0efd9cf1e5132f9f6098581faa3db4e2e489dd
Flazer214 <ivanpustovit786@gmail.com>
2025-05-27 19:55:32 +0300

    fix

--- src/api/v1/endpoints/sections.py
+++ src/api/v1/endpoints/sections.py
@@ -18,20 +18,20 @@ def get_section_by_id(section_id: UUID, db: Session = Depends(get_db)):
 
 @router.post("/")
 def add_section(course_id: UUID, payload: SectionCreate, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    if current_user.role not in [Role.TEACHER, Role.ADMIN]:
+    if current_user.role != Role.TEACHER:
         raise Unauthorized("Access for teacher only!")
     section = add_section_to_course(db, payload, course_id)
     return section
 
 @router.delete("/{section_id}")
 def delete_section(section_id: UUID, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    if current_user.role not in [Role.TEACHER, Role.ADMIN]:
+    if current_user.role != Role.TEACHER:
         raise Unauthorized("Access for teacher only!")
     section = information_about_section(db, section_id)
     return delete_section_from_course(db, section)
 
 @router.put("/{section_id}")
 def update_section(section: UUID, payload: SectionUpdate, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    if current_user.role not in [Role.TEACHER, Role.ADMIN]:
+    if current_user.role != Role.TEACHER:
         raise Unauthorized("Access for teacher only!")
     return update_info_about_section(db, section, payload, current_user = current_user)
