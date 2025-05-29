c23e50aeed12796b315aa0a65d1f503c008bdfa2
uasim1702 <uasim1702@abv.bg>
2025-05-29 21:05:24 +0300

    fix

--- src/api/v1/endpoints/teachers.py
+++ src/api/v1/endpoints/teachers.py
@@ -17,7 +17,7 @@ router = APIRouter()
 def list_pending_students_endpoint(
     course_id: UUID,
     db: Session = Depends(get_db),
-    current_teacher: UserModel = Depends(get_teacher_user())
+    current_teacher: UserModel = Depends(get_teacher_user)
 ):
     return list_pending_students(db=db, current_teacher=current_teacher, course_id=course_id)
 
